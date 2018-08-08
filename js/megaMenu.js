4859e6ae85f6c3f5ccf49bdf6b5d945ab16e0c89
Xemsssa <Xemsss@gmail.com>
2018-08-08 17:24:34 +0300

    -layouting header -some changes

--- js/megaMenu.js
+++ js/megaMenu.js
@@ -1,14 +1,14 @@
 // Mega menu slide down on hover with carousel
 
 $(document).ready(function(){
-    $(".dropdown").hover(            
+    $(".dropdown").on('click',        
         function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
-            $(this).toggleClass('open');        
+            $('.dropdownMenu', this).not('.in .dropdownMenu').stop(true,true).slideDown("400");
+            // $(this).toggleClass('open');        
         },
         function() {
             $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
-            $(this).toggleClass('open');       
+            // $(this).toggleClass('open');       
         }
     );
 });
