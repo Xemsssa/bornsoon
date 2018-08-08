58c292278d67f7d1916d67560563c88156e6aa55
Xemsssa <Xemsss@gmail.com>
2018-08-08 12:56:52 +0300

    -added svg -create main menu -add js for hover, but maybe better use hover -styling menu

---
+++ js/megaMenu.js
@@ -0,0 +1,14 @@
+// Mega menu slide down on hover with carousel
+
+$(document).ready(function(){
+    $(".dropdown").hover(            
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideDown("400");
+            $(this).toggleClass('open');        
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
+            $(this).toggleClass('open');       
+        }
+    );
+});
