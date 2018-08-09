014f737b3ae89336fe0f7fae669bd0f7ee4834b4
Xemsssa <Xemsss@gmail.com>
2018-08-09 16:36:42 +0300

    menu for mobile

--- js/megaMenu.js
+++ js/megaMenu.js
@@ -1,14 +1,10 @@
 // Mega menu slide down on hover with carousel
 
 $(document).ready(function(){
-    $(".dropdown").on('click',        
-        function() {
-            $('.dropdownMenu', this).not('.in .dropdownMenu').stop(true,true).slideDown("400");
-            // $(this).toggleClass('open');        
-        },
-        function() {
-            $('.dropdown-menu', this).not('.in .dropdown-menu').stop(true,true).slideUp("400");
-            // $(this).toggleClass('open');       
-        }
-    );
+    $(".header_hamburger").on('click', function() {
+        // console.log("clicked");
+        let navigation = $(".navigation");
+        // console.log(menu);
+        navigation.toggleClass("toggle");
+    })
 });
