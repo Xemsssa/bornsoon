21e11214081db4d89fbd97a6c5df79213b6b9c21
Xemsssa <Xemsss@gmail.com>
2018-08-20 12:41:19 +0300

    -layouting favorite icon -create functionality of fav -icon change on click

--- js/megaMenu.js
+++ js/megaMenu.js
@@ -1,23 +1,25 @@
 // Mega menu slide down on hover with carousel
 
-$(document).ready(function(){
+$(document).ready( () => {
+    // hamburgermobile menu
     $(".header_hamburger").on('click', onClick(".navigation"));
 
-
-    // todo
-    // $(".sub-title").on('click', onClick(".menu-block"));
-    $(".sub-title").on('click', function() {
-        // alert("clicked");
+    // show  submenu when click on title
+    $(".sub-title").on('click', function () {
         let that = this;
-
         let el = $(that).siblings().first();
-        // let el = $(that).siblings('ul');
-        // let el = $(that).siblings('ul:first');
-        // console.log("done " + el);
         el.toggle();
     });
     
+    // change icon when clicking on favorite icon
+    // $(".salesItem_favorite").on('click', onClick(".salesItem_icon"));
+    $(".salesItem_favorite").on('click', function (){
+        // alert("clicked");
+        let that = this;
+        $(that).toggleClass('fav');
+    });
 
+    // show menu block
     function onClick(element) {
         // console.log("clicked");
         let menu_block = $(element);
