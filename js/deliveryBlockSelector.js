8c2c12e66d3909e5b3f57ae1f88be1e0a3e79858
Xemsssa <Xemsss@gmail.com>
2018-09-04 15:17:29 +0300

    delivery js(jquery) selector

--- js/deliveryBlockSelector.js
+++ js/deliveryBlockSelector.js
@@ -1,9 +1,20 @@
 $(document).ready(() =>{
+    $(".delivery_type").on("click", (e) => {
+        // console.log("click");
 
-    $(".delivery_type").on("click", () => {
-        // alert("click");
-        $(this).addClass("delivery_selected");
+        // $(".delivery_icon").removeClass("hover");
 
-        $(this).siblings().removeClass("delivery_selected");
+        let el = $(e.currentTarget);
+
+        el.addClass("delivery_selected");
+        el.siblings().removeClass("delivery_selected");
+        
+
+        // console.log(el.children().find(".delivery_icon").css("filter", "opacity(1)"));
+        el.find(".delivery_icon").css("filter", "opacity(1)");
+        el.find(".delivery_icon").addClass("hover");
+
+        // el.siblings().find(".delivery_icon").removeClass("hover");
+        
     });
 });
